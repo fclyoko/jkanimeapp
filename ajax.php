905ee0e3b3a1225f4a9a2f9d05874fd761a4ca3f
nerdbastard <x@devhub.ninja>
2017-02-05 00:58:21 -0600

    Limpieza

--- ajax.php
+++ ajax.php
@@ -1,9 +1,13 @@
 <?php
 require_once("simple_html_dom.php");
 $url = $_POST["jkanimeurl"];
-$playertype = $_POST["playertype"];
+$playertype = $_POST["playertype"]; // 1 ? 2
 
 function curl($url) {
+	/**
+	 * @param string $url URL de JKANIME
+	 * @return  string HTML completo de la página
+	 */
     $curl = curl_init();
     curl_setopt ($curl, CURLOPT_URL, $url);
     curl_setopt ($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36");
@@ -17,6 +21,10 @@ function curl($url) {
     return $data;
 }
 function getVideo_iframe($response) {
+	/**
+	 * @param string $response HTML devuelto por Curl
+	 * @return string URL del iframe que contiene el reproductor de JKANIME
+	 */
 	$html=str_get_html($response);
 	foreach ($html->find('#the_video-1 iframe.player_conte') as $e) {
 		 $e->src;
@@ -25,6 +33,10 @@ function getVideo_iframe($response) {
 	return $iframe_url;
 }
 function get_Video($url) {
+	/**
+	 * @param string URL del iframe que contiene el reproductor de JKANIME
+	 * @return string Sección del código HTML que contiene las variables flash del reproductor
+	 */
 	$html=str_get_html($url);
 	foreach ($html->find('embed.player_conte') as $e) {
 
@@ -38,37 +50,25 @@ $iframe_url = getVideo_iframe($response);
 $response2 = curl($iframe_url);
 $flashvars = get_Video($response2);
 $subject = $flashvars;
-$pattern = '#file=http://jkanime.net/stream/(.*)$#';
+$pattern = '#file=http://jkanime.net/stream/(.*)$#'; //limpia la cadena para dejar solo la URL del mp4
 preg_match($pattern, $subject, $match);
 $url = str_replace('file=', '', $match[0]);
-//$file = "video.php?url=$url";
 
-if($playertype == 1) {
-	$output = printf(
-				'<div id="jwreproductor">Cargando...</div>
-					<script type="text/javascript">
-						jwplayer("jwreproductor").setup({
-  							"file": "%1$s",
-  							"height": 450,
-  							"width": 800,
-  							"autostart": false,
-  							"type": "video/mp4"
-						});
-					</script>
-				</html>',
-				//$file
-        $url
-			);
-} else {
-	$output =  printf(
-				'<div class="html5player">
-					<video controls="controls">
-						<source src="%1$s" type="video/mp4">
-					</video>
-				</div>',
-				$url
-			);
-}
+$output = ($playertype == 1) ? printf(
+'<div id="jwreproductor">Cargando...</div>
+<script type="text/javascript">
+	jwplayer("jwreproductor").setup({
+  		"file": "%1$s",
+  		"height": 450,
+  		"width": 800,
+  		"autostart": false,
+  		"type": "video/mp4"
+	});
+</script>', $url) : printf(
+'<div class="html5player">
+	<video controls="controls">
+		<source src="%1$s" type="video/mp4">
+	</video>
+</div>', $url);
 echo $output;
-
 ?>
